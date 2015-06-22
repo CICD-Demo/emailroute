89aebfd3947f681d2108fbd940ce52bdb67b8a9a
Jim Minter <jminter@redhat.com>
2015-06-22 01:24:04 -0400

    rename broker service

--- deploy.sh
+++ deploy.sh
@@ -55,10 +55,12 @@ items:
               - containerPort: 8778
                 name: jolokia
               env:
+              - name: JAVA_OPTS
+                value: "-server -XX:+UseCompressedOops -verbose:gc -Xloggc:/opt/eap/standalone/log/gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=3M -XX:-TraceClassUnloading -Xms128m -Xmx512m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true -Djboss.modules.system.pkgs=org.jboss.logmanager -Djava.awt.headless=true -Djboss.modules.policy-permissions=true -Xbootclasspath/p:/opt/eap/jboss-modules.jar:/opt/eap/modules/system/layers/base/org/jboss/logmanager/main/jboss-logmanager-1.5.4.Final-redhat-1.jar:/opt/eap/modules/system/layers/base/org/jboss/logmanager/ext/main/javax.json-1.0.4.jar:/opt/eap/modules/system/layers/base/org/jboss/logmanager/ext/main/jboss-logmanager-ext-1.0.0.Alpha2-redhat-1.jar -Djava.util.logging.manager=org.jboss.logmanager.LogManager -javaagent:/opt/eap/jolokia.jar=port=8778,host=0.0.0.0,discoveryEnabled=false"
               - name: MQ_SERVICE_PREFIX_MAPPING
                 value: amq
               - name: AMQ_TCP_SERVICE_HOST
-                value: broker
+                value: amqbroker
               - name: AMQ_TCP_SERVICE_PORT
                 value: "61616"
               - name: amq_JNDI
@@ -70,4 +72,20 @@ items:
         labels:
           service: emailroute
           function: application
+
+# not actually required, but makes UI prettier in beta4
+- kind: Service
+  apiVersion: v1beta3
+  metadata:
+    name: emailroute
+    labels:
+      service: emailroute
+      function: application
+  spec:
+    ports:
+    - port: 80
+      targetPort: 8080
+    selector:
+      service: emailroute
+      function: application
 EOF
